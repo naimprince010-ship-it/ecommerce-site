3bcce706a5b188a2ba6affd1885105b5ce6cb912
naimprince010-ship-it <naimprince010@gmail.com>
2025-11-15 01:51:39 -0800

    Add homepage for e-commerce site

---
+++ client/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>E-commerce Site</title>
+      </Head>
+      <main>
+        <h1>Welcome to our e-commerce site</h1>
+        <p>Your one-stop shop for medications and health products.</p>
+      </main>
+    </>
+  );
+}
