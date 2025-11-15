f22185de7efd7fbe7cfb956b7bf8351f9bed4824
naimprince010-ship-it <naimprince010@gmail.com>
2025-11-15 02:26:23 -0800

    Add cart page

---
+++ client/pages/cart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CartPage = () => {
+  return (
+    <main>
+      <h1>Shopping Cart</h1>
+      <p>Your cart is currently empty.</p>
+    </main>
+  );
+};
+
+export default CartPage;
