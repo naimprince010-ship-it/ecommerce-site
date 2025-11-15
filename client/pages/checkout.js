18fee86dede71896f135867c7143e43ff3cabe04
naimprince010-ship-it <naimprince010@gmail.com>
2025-11-15 02:27:05 -0800

    Add checkout page

---
+++ client/pages/checkout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+  return (
+    <main>
+      <h1>Checkout</h1>
+      <p>Please proceed with your order details.</p>
+    </main>
+  );
+};
+
+export default CheckoutPage;
